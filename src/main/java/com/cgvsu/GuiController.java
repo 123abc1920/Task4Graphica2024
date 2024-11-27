0d7b9f044d1c37274add071b2bbeb53a7332db85
123abc1920 <123abc1920@mail.ru>
2024-11-27 15:24:12 +0300

    update test structure. Tests are in src/test/java/

--- src/main/java/com/cgvsu/GuiController.java
+++ src/main/java/com/cgvsu/GuiController.java
@@ -136,6 +136,7 @@ public class GuiController {
         try {
             String fileContent = Files.readString(fileName);
             mesh = ObjReader.read(fileContent);
+            
             // todo: обработка ошибок
         } catch (IOException exception) {
 
